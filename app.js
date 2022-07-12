9c34ba0d1c2c4d2031983eee607516c36301b3ca
Transistor1001 <alex.1001@hotmail.es>
2022-07-12 11:39:48 -0300

    con varias alternativas

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Hecho por mi
 function upDate(previewPic) {
     // cambia el texto
     var filtro= previewPic.alt
@@ -26,3 +27,87 @@ function unDo() {
     div.style.backgroundImage = "url('')";
     document.getElementById('image').innerHTML='Hover over an image below to display here.';
 }
+
+
+
+
+
+
+
+//otras maneras mas eficiente de hacerlo
+
+
+/*
+actualización de la función (imagen de vista previa) {
+    x=document.getElementById('imagen');
+    x.innerHTML=imagen previa.alt;
+    x.style.backgroundImage='url('+previewPic.src+')';
+}
+
+función deshacer(){
+    x=document.getElementById('imagen');
+	x.innerHTML="Pase el cursor sobre una imagen para mostrarla aquí";
+    x.style.backgroundImage="url('')";
+}
+*/
+
+
+
+
+/*
+function upDate(previewPic){    
+    document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")";
+    document.getElementById('image').innerHTML = previewPic.alt;
+     
+       }
+   
+       function unDo(){
+    document.getElementById('image').style.backgroundImage = "url('')";
+    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+       
+       }
+*/
+
+
+
+
+
+/*
+function upDate(previewPic){
+}
+   
+function upDate(previewPic){
+   var src = previewPic.getAttribute( "src" );
+   var alt = previewPic.getAttribute( "alt" );
+   document.getElementById('image').style.backgroundImage = "url('" + src + "')";
+   document.getElementById('image').innerHTML = alt;
+}
+
+	function unDo(){
+	}
+
+function unDo(X){
+   X = document.getElementById('image');
+   X.style.backgroundImage = "url('')";
+    document.getElementById('image').innerHTML = "Hover over an image below to display here";
+}
+*/
+
+
+
+
+
+
+/*
+function upDate(element){
+  document.getElementById('image').innerHTML= element.alt;
+  image.style.backgroundImage = "url("+element.src+")";
+  //document.getElementById('image').innerHTML = "<img src='"+element.src+"'>";
+ //'url(http://localhost/background.png)'
+}
+
+function unDo(element){
+  document.getElementById('image').innerHTML = "Hover over an image below to display here."
+  image.style.backgroundImage = "url()";
+}
+*/
